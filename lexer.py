6528cdf86d558a01759a7adbbf30556647c605a4
Sephirothxlx <linxuan@uchicago.edu>
2019-10-18 13:02:35 -0700

    some changes in lexer

--- lexer.py
+++ lexer.py
@@ -1,30 +1,34 @@
 import ply.lex as lex
 
 reserved = {
-    'int' : 'int',
-    'cint' : 'cint',
-    'float' : 'float',
-    'bool' : 'bool',
-    'void' : 'void',
-    'ref' : 'ref',
-    'noalias' : 'noalias',
-    'return' : 'return',
-    'while' : 'while',
-    'if' : 'if',
-    'else' : 'else',
-    'print' : 'print',
-    'def' : 'def',
-    "extern" : 'extern'
+    'int' : 'INT',
+    'cint' : 'CINT',
+    'float' : 'FLOAT',
+    'bool' : 'BOOL',
+    'void' : 'VOID',
+    'ref' : 'REF',
+    'noalias' : 'NOALIAS',
+    'return' : 'RETURN',
+    'while' : 'WHILE',
+    'if' : 'IF',
+    'else' : 'ELSE',
+    'print' : 'PRINT',
+    'def' : 'DEF',
+    "extern" : 'EXTERN',
+    "true" : 'TRUE',
+    "false" : 'FALSE'
  }
 
 tokens = list(reserved.values()) + [
+    #number and boolean
+    'NUMBER', 'FNUMBER',
     # arithmetic
     'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'ASSIGN',
     # compare
     'EQUAL', 'GREATERTHAN', 'SMALLERTHAN', 
     # logical operations
     'AND', 'OR', 'NEGATE',
-    # (),{},[], see https://www.cis.upenn.edu/~matuszek/General/JavaSyntax/PARENTHESES.html
+    # (),{},[]
     'LPARENTHESES', 'RPARENTHESES', 'LBRACE', 'RBRACE', 'LBRACKET', 'RBRACKET',
     # delimiter
     'COMMA', 'SEMICOLON',
@@ -68,16 +72,26 @@ def t_error(t):
     t.lexer.skip(1)
 
 def t_slit(t):
-    r'[^"\n\r]*"'
+    r'[^"\n\r]*'
     return t
 
 def t_ident(t):
-    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    r'[a-zA-Z_]+[a-zA-Z_0-9]*'
     t.type = reserved.get(t.value, "ident")
     return t
 
 def t_varid(t):
     r'\$[a-zA-Z_][a-zA-Z_0-9]*'
     return t
+
+def t_NUMBER(t):
+    r'[-+]?\d+'
+    t.value = int(t.value)
+    return t
+
+def t_FNUMBER(t):
+    r'[-+]?[0-9]*\.?[0-9]+'
+    t.value = float(t.value)
+    return t 
 
 lexer = lex.lex()
